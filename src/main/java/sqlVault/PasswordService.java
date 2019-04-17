ed651d79419ecd6929a8ceb0d4082e9c645482e0
Alex Markules <amarlar2354@gmail.com>
2019-04-16 22:22:07 -0400

    quick change to make the webpage load

--- src/main/java/sqlVault/PasswordService.java
+++ src/main/java/sqlVault/PasswordService.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RequestMapping
-@Controller
 public class PasswordService {
 	@RequestMapping(path = "pw", method = RequestMethod.GET)
 	@ResponseBody
